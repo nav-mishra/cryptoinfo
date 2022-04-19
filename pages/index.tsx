e05822762d5a78c6c39a379d991dca3ea732f51a
Dinesh Kumar <codinesh@live.com>
2022-04-20 01:15:28 +0530

    build fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -46,8 +46,8 @@ const HomePage: NextPage<InferGetStaticPropsType<typeof getStaticProps>> = (prop
       </section>
 
       {
-        props.projects.map(p =>
-          <div className='divide-y-2 border-md border-red-400'>
+        props.projects.map((p, i) =>
+          <div key={i} className='divide-y-2 border-md border-red-400'>
             <div className=" py-4 flex flex-wrap md:flex-nowrap ">
               <div className="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
                 <span className="font-semibold title-font ">{p.source}</span>
@@ -57,7 +57,7 @@ const HomePage: NextPage<InferGetStaticPropsType<typeof getStaticProps>> = (prop
                 <h2 className="text-2xl font-medium  title-font mb-2"><Link href={p.url}><a>{p.title}</a></Link></h2>
                 <p className="leading-relaxed">Glossier echo park pug, church-key sartorial biodiesel vexillologist pop-up snackwave ramps cornhole. Marfa 3 wolf moon party messenger bag selfies, poke vaporware kombucha lumbersexual pork belly polaroid hoodie portland craft beer.</p>
                 <a className="text-indigo-700 inline-flex items-center mt-4">Learn More
-                  <svg className="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                  <svg className="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2" fill="none" strokeLinecap="round" strokeLinejoin="round">
                     <path d="M5 12h14"></path>
                     <path d="M12 5l7 7-7 7"></path>
                   </svg>
@@ -66,7 +66,7 @@ const HomePage: NextPage<InferGetStaticPropsType<typeof getStaticProps>> = (prop
             </div>
           </div>)
       }
-    </section>
+    </section >
   )
 }
 
